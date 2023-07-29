0e7557e17752900804ad0ec8345ca4eb9bba1dba
Weiqiang Li <lwq617597704@gmail.com>
2023-07-29 02:38:04 -0400

    improve the profile card

--- src/components/About.js
+++ src/components/About.js
@@ -3,10 +3,23 @@ import Profile from './Profile';
 
 const About = () => {
     return (
-        <section id="about_me" className='flex px-2 sm:px-4 pt-2.5 pb-16'>
-                <div>
+        <section id="about_me" className='flex py-20 sm:px-4 bg-neutral-700  border-gray-200 border-b-2 justify-center'>
+            <div className='rounded-lg container mx-16 flex flex-col bg-white justify-center h-1/2 w-2/3'>
+                <h4 className='text-center bg-gradient-to-br from-gray-300 to-gray-100 rounded-t-lg border-gray-300 border-b-2'>About Me</h4>
+
+                <div className='flex'>
                     <Profile />
+                    <div className="bg-slate-100 flex flex-grow">
+                        <p>Place holder</p>
+                        <p>Place holder</p>
+                        <p>Place holder</p>
+                        <p>Place holder</p>
+                    </div>
                 </div>
+                <div>
+                    <p>Skill</p>
+                </div>
+            </div>
 
 
         </section>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,23 +1,45 @@
 import React from 'react'
 import my_photo from '../image/male.svg'
+import { BsInstagram, BsLinkedin, BsGithub } from 'react-icons/bs'
+import { AiOutlineMail } from 'react-icons/ai'
 
 const Profile = () => {
     return (
-        <article className='rounded-lg overflow-hidden outline-2 bg-white drop-shadow-md'>
-            <div className="bg-project md:p-8">
-                <div className="drop-shadow-xl transition ease-in-out delay-150 hover:scale-125 duration-500 cursor: cursor-zoom-in">
-                    <img className="rounded-full" src={my_photo} alt='' />
+        <article className='overflow-hidden outline-2 drop-shadow-md bg-slate-200 border-gray-300 border-r-2 flex place-content-stretch'>
+            <div className='flex-col flex-none'>
+                <div className="px-8 pt-8 bg-gradient-to-b from-violet-500 ">
+                    <div className="drop-shadow-xl transition ease-in-out delay-150 hover:scale-125 duration-500 cursor: cursor-zoom-in">
+                        <img className="rounded-full border-red-500 border-2 bg-white" src={my_photo} alt='' />
+                    </div>
                 </div>
-            </div>
 
-            <div className="p-4 flex flex-col">
-                <h4 className="font-squada text-2xl text-gray-700 gap-2">Weiqiang Li</h4>
-                <div className="flex flex-wrap justify-between items-center gap-2">
-                    <ul className='flex flex-col gap-1'>
-                        <li>Phone: (857)452-9414</li>
-                        <li>Email: weiqli1023@gmail.com</li>
-                        <li>Linekdin: </li>
+                <div className="p-4 flex flex-col">
+                    <h4 className="font-squada text-2xl text-gray-700 gap-2 text-center pb-1">Weiqiang Li</h4>
+                    <p className='pb-4 text-center'>Boston, MA</p>
+                    <div className='just_a_line border-t-2 border-black pb-5'></div>
+                    <ul className='flex space-x-4 justify-center pb-5'>
+                        <li>
+                            <a href="google.com">
+                                <BsInstagram size={30} className={"transition ease-in-out delay-150 hover:-translate-y-2 duration-300"}/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="google.com">
+                                <AiOutlineMail size={30} className={"transition ease-in-out delay-150 hover:-translate-y-2 duration-300"}/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="google.com">
+                                <BsLinkedin size={30} className={"transition ease-in-out delay-150 hover:-translate-y-2 duration-300"}/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="google.com">
+                                <BsGithub size={30} className={"transition ease-in-out delay-150 hover:-translate-y-2 duration-300"}/>
+                            </a>
+                        </li>
                     </ul>
+                    <div className='just_a_line border-t-2 border-black'></div>
                 </div>
             </div>
         </article>
