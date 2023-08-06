02b5394185570d3931720e5773db6f7885463c63
Weiqiang Li <lwq617597704@gmail.com>
2023-08-06 00:47:02 -0400

    Completed Hero section, added new animated button

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,28 +1,31 @@
 import React from 'react'
 import Typed from 'react-typed'
-
+import {BsArrowDownCircle} from 'react-icons/bs'
 const Hero = () => {
     return (
         <section id="hero" className='flex px-2 sm:px-4 h-screen'>
-            <div className="container mx-auto flex-col flex px-2 py-12 items-center">
-                <Typed
-                    style={{ color: "rgb(14 116 144 / 1)", fontSize: "96px" }}
-                    strings={['WELCOME!']}
-                    typeSpeed={230}
-                    backSpeed={100}
-                    showCursor={false}
-                />
-
-                <h2 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6 text-white">Grow with data</h2>
-                <div>
-                    <p className='md:text-5xl sm:{text-4xl text-xl font-bold text-white'>fast, flexible</p>
+            <div className="container mx-auto flex-col flex px-2 pb-12 pt-8 items-center relative space-y-8">
+                <h1 className='capitalize text-[96px] text-cyan-600'>welcome!</h1>
+          
+                <h2 className="text-5xl pb-6 text-white">
+                    <span className="wave text-[50px] pr-2">👋</span>
+                    Hi, I'm 
+                    <text className=' bg-gray-400 text-black ml-2 my-4 px-2'>Weiqiang Li</text>
+                </h2>
+                <div className=''>
                     <Typed
-                        style={{ color: 'white' }}
-                        strings={['BTB', 'BTC', 'SASS']}
-                        typeSpeed={120}
-                        backSpeed={140} loop
+                        style={{ fontSize: "48px", color: 'transparent', WebkitTextStroke: "2px #e73c7e"}}
+                        strings={['Software Engineer', 'Web Developer', 'Front-End', 'Back-End', 'Full-Stack']}
+                        typeSpeed={100}
+                        backSpeed={80}
+                        cursorChar= "_" 
+                        loop
                     />
                 </div>
+ 
+                <a href="#about" className= "text-white absolute bottom-1/4">
+                    <BsArrowDownCircle size={50} className='arrow-down'/>
+                </a>
             </div>
         </section>
     )
