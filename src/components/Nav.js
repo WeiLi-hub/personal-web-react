51186556fbacc17bdaafa3d4a5ee6b62ce5bc759
Weiqiang Li <lwq617597704@gmail.com>
2023-09-12 02:27:26 -0400

    Working on scroll effect with Navbar styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,29 @@
-import { React } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Nav = () => {
-    // const [color, setColor] = useState(false);
-    // const changeColor = () =>{
-    //     if(window.scrollY >= 90){
-    //         setColor(true);
-    //     }
-    //     else{
-    //         setColor(false);
-    //     }
-    // }
+    const [scrolling, setScrolling] = useState(false);
+    const listenScrollEvent = () => {
+        if (window.scrollY >= 90) {
+            setScrolling(true);
+            console.log("scrolling");
+        }
+        else {
+            setScrolling(false);
+            console.log("didnt scroll");
+        }
+    }
+    useEffect(() => {
+        window.addEventListener("scroll", listenScrollEvent);
+        return () => {
+            window.removeEventListener("scroll", listenScrollEvent);
+        };
+    }, []);
 
-    // window.addEventListener('scroll', changeColor);
 
     return (
         <nav className="navbar px-2 sm:px-4 py-2 items-center sticky top-0 bg-black/[0.7] z-10 text-gray-200 transition-[0.4]">
             <div className="container flex flex-wrap justify-center mx-auto">
-                <ul className="flex flex-col p-4 mt-4 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-base md:font-bold md:border-0">
+                <ul className="flex flex-col rounded-full bg-white border transition-[0.3] px-8 py-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-base md:font-bold md:border-0"> 
                     <li className="navlink">
                         <a
                             href="#home"
