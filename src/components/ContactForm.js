828a11f11cec76268f809b836af8c4a6671bd413
Weiqiang Li <lwq617597704@gmail.com>
2023-11-18 12:52:26 -0500

    minor changed

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -44,15 +44,15 @@ const ContactForm = () => {
         <form ref={form} id={'contact-form'} onSubmit={sendEmail}>
             <ToastContainer />
             <div className="h-3/4 w-1/2 backdrop-blur-lg backdrop-brightness-110 top-[12.5%] left-1/4 absolute flex flex-col py-8 justify-between shadow-2xl">
-                <h1 className='mt-8 ml-[15%] text-white font-extrabold text-4xl'>Contact Me</h1>
+                <h1 className='mt-8 ml-[15%] text-gray-700 font-extrabold text-4xl'>Contact Me</h1>
                 <div className="flex mx-[15%] after:content-['*'] after:ml-1 after:text-red-500 items-center">
-                    <input className="mt-1 px-3 py-2 bg-gray-400 border-1 shadow-sm border-black placeholder-white focus:outline-none focus:border-red-500 focus:ring-red-500 focus:ring-2 w-full" autoComplete="off" type="text" name="user_name" placeholder="Your name" required />
+                    <input className="mt-1 px-3 py-2 bg-gray-400 border-1 shadow-sm border-black placeholder-white focus:outline-none focus:border-black focus:ring-black focus:ring-2 w-full" autoComplete="off" type="text" name="user_name" placeholder="Your name" required />
                 </div>
                 <div className="flex mx-[15%] after:content-['*'] after:ml-1 after:text-red-500 items-center">
-                    <input className='mt-1 px-3 py-2  bg-gray-400 border-1 shadow-sm border-black placeholder-white focus:outline-none focus:border-red-500 focus:ring-red-500 focus:ring-2 w-full' autoComplete="off" type="email" name="user_email" placeholder="You@example.com" required />
+                    <input className='mt-1 px-3 py-2  bg-gray-400 border-1 shadow-sm border-black placeholder-white focus:outline-none focus:border-black focus:ring-black focus:ring-2 w-full' autoComplete="off" type="email" name="user_email" placeholder="You@example.com" required />
                 </div>
                 <div className="flex mx-[15%] after:content-['*'] after:ml-1 after:text-red-500 items-center">
-                    <textarea className='mt-1 px-3 py-2  bg-gray-400 border-1 shadow-sm border-black placeholder-white focus:outline-none focus:border-red-500 focus:ring-red-500 focus:ring-2 w-full' style={{ resize: "none" }} name="message" placeholder="Leave me a message....." required />
+                    <textarea className='mt-1 px-3 py-2  bg-gray-400 border-1 shadow-sm border-black placeholder-white focus:outline-none focus:border-black focus:ring-black focus:ring-2 w-full' style={{ resize: "none" }} name="message" placeholder="Leave me a message....." required />
                 </div>
                 <div className='flex flex-row-reverse mr-[17%]'>
                     <ReactiveButton color="primary" size="large" shadow idleText="Submit" type = {'submit'}/>
