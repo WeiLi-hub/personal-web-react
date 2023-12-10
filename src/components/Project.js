d065cb8986c273ae50b73fa4d2ec986c6e6a73b5
Weiqiang Li <lwq617597704@gmail.com>
2023-12-10 03:00:52 -0500

    New style of project section

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import ProjectLeft from './Project_left';
-import Website from '../image/project_personalwebsite.png'
-// import example from '../image/bg_about.jpg'
+// import Website from '../image/project_personalwebsite.png'
+import example from '../image/bg_about.jpg'
+import ProjectRight from './Project_right';
 
 const Project = () => {
     return (
         <section id="project" className="py-20 relative flex flex-col font-sans items-center">
             <div className='flex pb-8 justify-center'>
-                <h1 className='header text-white text-center font-bold font-serif text-[54px]'>
+                <h1 className='header text-gray-300 text-center font-bold font-serif text-[54px]'>
                     Projects
                 </h1>
             </div>
-            <ProjectLeft image={Website} name="Personal Porfolio" description="This is description" href="https://github.com/WeiLi-hub" />
+            <ProjectLeft image={example} name="Personal Porfolio" tech={["React.js", "TailwindCSS", "TailwindCSS", "TailwindCSS", "TailwindCSS"]}
+                description=" Designed and developed a mobile and desktop-responsive portfolio website with a dynamic and interactive
+            user interface.
+            Created custom, reusable, and responsive UI components using React.js and Tailwind CSS.
+            Implemented smooth, parallax scrolling effects and leveraged Framer Motion for scroll-driven animations.
+            Optimized the web application performance and maintainability by applying modern React design patterns
+            and integrating third-party libraries" href="https://github.com/WeiLi-hub" />
+
+            <ProjectRight image={example} name="Personal Porfolio" tech={["React.js", "TailwindCSS", "TailwindCSS", "TailwindCSS", "TailwindCSS"]}
+                description=" Designed and developed a mobile and desktop-responsive portfolio website with a dynamic and interactive
+            user interface.
+            Created custom, reusable, and responsive UI components using React.js and Tailwind CSS.
+            Implemented smooth, parallax scrolling effects and leveraged Framer Motion for scroll-driven animations.
+            Optimized the web application performance and maintainability by applying modern React design patterns
+            and integrating third-party libraries" href="https://github.com/WeiLi-hub" />
 
         </section>
     )
--- src/components/Project_left.js
+++ src/components/Project_left.js
@@ -3,19 +3,28 @@ import { BsGithub } from 'react-icons/bs';
 
 const Project_left = (props) => {
     return (
-        <div className='flex justify-center m-8 text-white w-full px-20'>
-            <div className='flex flex-1 justify-center items-center'>
-                <img src={props.image} alt="website" className='rounded-3xl w-[500px] h-[300px] max-h-[80%] max-w-full border-white border-2 object-contain' />
+        <div className='flex justify-center mt-8 mb-10 text-gray-300 w-full px-20 font-semibold'>
+            <div className='flex flex-1 justify-center items-center pr-12'>
+                <img src={props.image} alt="website" className='rounded-3xl max-h-[80%] object-contain' />
             </div>
-            <div className='flex flex-1 flex-col items-center'>
-                <h2>{props.name}</h2>
-                <p>{props.description}</p>
- 
-                <a href={props.href} target="_blank" rel='noreferrer' className='flex align-middle mt-auto bg-black border border-gray-500 hover:bg-opacity-20
-                 text-gray-300 hover:bg-black py-3 px-10 transition ease-in-out duration-200'>
-                    <BsGithub size = {25} className=' pr-2'/>
-                    Repo
-                </a>
+            <div className='flex flex-1 flex-col pl-12'>
+                <h2 className = "font-bold text-3xl mb-2">{props.name}</h2>
+                <div className='flex flex-wrap gap-2'>
+                    {(props.tech).map(element => {
+                        return (
+                            <div className='bg-slate-400 text-black px-2 py-1'>{element}</div>
+                        );
+                    })}
+                </div>
+                <p className='mt-4 my-8 text-justify'>{props.description}</p>
+
+                <div className='flex mt-auto'>
+                    <a href={props.href} target="_blank" rel='noreferrer' className='flex bg-black border border-gray-300 
+                 text-gray-300 hover:bg-gray-300 py-3 px-10 transition ease-in-out duration-700 hover:text-black'>
+                        <BsGithub size={25} className='pr-2' />
+                        Repo
+                    </a>
+                </div>
             </div>
         </div>
     );
